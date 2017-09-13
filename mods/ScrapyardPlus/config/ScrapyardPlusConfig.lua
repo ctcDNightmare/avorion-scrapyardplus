5546f5eb3495b613c9dbfdef9a6a78957d8f9f93
dnightmare <sebi@ctc-gaming.com>
2017-09-14 00:50:15 +0200

    Add Configuration skeleton and screenshot

---
+++ mods/ScrapyardPlus/config/ScrapyardPlusConfig.lua
@@ -0,0 +1,8 @@
+local ScrapyardPlusConfig = {}
+
+ScrapyardPlusConfig.version = "[0.1.0]"
+ScrapyardPlusConfig.ModName = "[ScrapyardPlus]"
+ScrapyardPlusConfig.AllowLifetime = false
+ScrapyardPlusConfig.AlliancePriceFactor = 1.5
+
+return ScrapyardPlusConfig
